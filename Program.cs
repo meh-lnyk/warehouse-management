2e3a7525b949604e3babe3912a4908d367979aa3
Nika Kurafeeva <nikurafeeva@gmail.com>
2025-05-25 23:31:13 +0300

    добавлена базовая логика редактирования коробок

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace WarehouseApp
 {
@@ -53,32 +54,103 @@ namespace WarehouseApp
             }
         }
 
-        static void Main()
+        static void EditBoxes(List<Pallet> pallets)
         {
-            Console.WriteLine("Добро пожаловать в систему управления складом!");
-            Console.WriteLine("Выберите действие:");
-            Console.WriteLine("1. Посмотреть все паллеты и их содержимое");
-            Console.WriteLine("2. Редактировать коробки (добавить, изменить или удалить)");
-            Console.WriteLine("0. Выход");
+            Console.WriteLine("\nРедактирование коробок:");
+            Console.WriteLine("1. Добавить коробку");
+            Console.WriteLine("2. Удалить коробку");
+            Console.WriteLine("0. Назад");
 
-            Console.Write("Ваш выбор: ");
-            string input = Console.ReadLine();
+            Console.Write("Выберите действие: ");
+            string choice = Console.ReadLine();
 
-            switch (input)
+            switch (choice)
             {
                 case "1":
-                    var pallets = GenerateSamplePallets();
-                    DisplayPallets(pallets);
+                    Console.WriteLine("Выберите паллету (введите номер от 1 до " + pallets.Count + "):");
+                    for (int i = 0; i < pallets.Count; i++)
+                        Console.WriteLine($"{i + 1}. ID: {pallets[i].Id}");
+
+                    if (int.TryParse(Console.ReadLine(), out int index) && index > 0 && index <= pallets.Count)
+                    {
+                        var pallet = pallets[index - 1];
+                        var newBox = new Box();
+
+                        Console.Write("Ширина: "); newBox.Width = double.Parse(Console.ReadLine());
+                        Console.Write("Высота: "); newBox.Height = double.Parse(Console.ReadLine());
+                        Console.Write("Глубина: "); newBox.Depth = double.Parse(Console.ReadLine());
+                        Console.Write("Вес: "); newBox.Weight = double.Parse(Console.ReadLine());
+
+                        Console.Write("Дата производства (гггг-мм-дд): ");
+                        if (DateTime.TryParse(Console.ReadLine(), out var date))
+                            newBox.ManufactureDate = date;
+
+                        pallet.Boxes.Add(newBox);
+                        Console.WriteLine("Коробка добавлена.");
+                    }
+                    else Console.WriteLine("Неверный выбор паллеты.");
                     break;
+
                 case "2":
-                    Console.WriteLine("[Здесь будет логика редактирования коробок]");
+                    Console.WriteLine("Выберите паллету:");
+                    for (int i = 0; i < pallets.Count; i++)
+                        Console.WriteLine($"{i + 1}. ID: {pallets[i].Id}");
+
+                    if (int.TryParse(Console.ReadLine(), out int delIndex) && delIndex > 0 && delIndex <= pallets.Count)
+                    {
+                        var pallet = pallets[delIndex - 1];
+                        for (int i = 0; i < pallet.Boxes.Count; i++)
+                            Console.WriteLine($"{i + 1}. Коробка {i + 1}, вес: {pallet.Boxes[i].Weight} кг");
+
+                        Console.Write("Введите номер коробки для удаления: ");
+                        if (int.TryParse(Console.ReadLine(), out int boxIndex) && boxIndex > 0 && boxIndex <= pallet.Boxes.Count)
+                        {
+                            pallet.Boxes.RemoveAt(boxIndex - 1);
+                            Console.WriteLine("Коробка удалена.");
+                        }
+                        else Console.WriteLine("Неверный номер коробки.");
+                    }
+                    else Console.WriteLine("Неверный выбор паллеты.");
                     break;
+
                 case "0":
-                    Console.WriteLine("Завершение работы...");
-                    break;
+                    return;
                 default:
-                    Console.WriteLine("Неизвестный выбор. Попробуйте снова.");
+                    Console.WriteLine("Неверный выбор.");
                     break;
+            }
+        }
+
+        static void Main()
+        {
+            var pallets = GenerateSamplePallets();
+
+            while (true)
+            {
+                Console.WriteLine("\nДобро пожаловать в систему управления складом!");
+                Console.WriteLine("Выберите действие:");
+                Console.WriteLine("1. Посмотреть все паллеты и их содержимое");
+                Console.WriteLine("2. Редактировать коробки (добавить или удалить)");
+                Console.WriteLine("0. Выход");
+
+                Console.Write("Ваш выбор: ");
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        DisplayPallets(pallets);
+                        break;
+                    case "2":
+                        EditBoxes(pallets);
+                        break;
+                    case "0":
+                        Console.WriteLine("Завершение работы...");
+                        return;
+                    default:
+                        Console.WriteLine("Неизвестный выбор. Попробуйте снова.");
+                        break;
+                }
             }
         }
     }
