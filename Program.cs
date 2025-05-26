07ed7f25829c23b18cfc0ca1d9e1df3d0ccd7f69
Nika Kurafeeva <nikurafeeva@gmail.com>
2025-05-26 20:00:26 +0300

    добавлена валидация ввода при добавлении коробки и разделение логики

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Globalization;
 
 namespace WarehouseApp
 {
@@ -67,58 +68,92 @@ namespace WarehouseApp
             switch (choice)
             {
                 case "1":
-                    Console.WriteLine("Выберите паллету (введите номер от 1 до " + pallets.Count + "):");
-                    for (int i = 0; i < pallets.Count; i++)
-                        Console.WriteLine($"{i + 1}. ID: {pallets[i].Id}");
-
-                    if (int.TryParse(Console.ReadLine(), out int index) && index > 0 && index <= pallets.Count)
-                    {
-                        var pallet = pallets[index - 1];
-                        var newBox = new Box();
-
-                        Console.Write("Ширина: "); newBox.Width = double.Parse(Console.ReadLine());
-                        Console.Write("Высота: "); newBox.Height = double.Parse(Console.ReadLine());
-                        Console.Write("Глубина: "); newBox.Depth = double.Parse(Console.ReadLine());
-                        Console.Write("Вес: "); newBox.Weight = double.Parse(Console.ReadLine());
-
-                        Console.Write("Дата производства (гггг-мм-дд): ");
-                        if (DateTime.TryParse(Console.ReadLine(), out var date))
-                            newBox.ManufactureDate = date;
-
-                        pallet.Boxes.Add(newBox);
-                        Console.WriteLine("Коробка добавлена.");
-                    }
-                    else Console.WriteLine("Неверный выбор паллеты.");
+                    AddBoxToPallet(pallets);
                     break;
-
                 case "2":
-                    Console.WriteLine("Выберите паллету:");
-                    for (int i = 0; i < pallets.Count; i++)
-                        Console.WriteLine($"{i + 1}. ID: {pallets[i].Id}");
-
-                    if (int.TryParse(Console.ReadLine(), out int delIndex) && delIndex > 0 && delIndex <= pallets.Count)
-                    {
-                        var pallet = pallets[delIndex - 1];
-                        for (int i = 0; i < pallet.Boxes.Count; i++)
-                            Console.WriteLine($"{i + 1}. Коробка {i + 1}, вес: {pallet.Boxes[i].Weight} кг");
-
-                        Console.Write("Введите номер коробки для удаления: ");
-                        if (int.TryParse(Console.ReadLine(), out int boxIndex) && boxIndex > 0 && boxIndex <= pallet.Boxes.Count)
-                        {
-                            pallet.Boxes.RemoveAt(boxIndex - 1);
-                            Console.WriteLine("Коробка удалена.");
-                        }
-                        else Console.WriteLine("Неверный номер коробки.");
-                    }
-                    else Console.WriteLine("Неверный выбор паллеты.");
+                    RemoveBoxFromPallet(pallets);
                     break;
-
                 case "0":
                     return;
                 default:
                     Console.WriteLine("Неверный выбор.");
                     break;
             }
+        }
+
+        static void AddBoxToPallet(List<Pallet> pallets)
+        {
+            Console.WriteLine("Выберите паллету (введите номер от 1 до " + pallets.Count + "):");
+            for (int i = 0; i < pallets.Count; i++)
+                Console.WriteLine($"{i + 1}. ID: {pallets[i].Id}");
+
+            if (int.TryParse(Console.ReadLine(), out int index) && index > 0 && index <= pallets.Count)
+            {
+                var pallet = pallets[index - 1];
+
+                try
+                {
+                    Console.Write("Ширина: ");
+                    if (!double.TryParse(Console.ReadLine(), out double width)) throw new Exception("Неверный формат ширины.");
+
+                    Console.Write("Высота: ");
+                    if (!double.TryParse(Console.ReadLine(), out double height)) throw new Exception("Неверный формат высоты.");
+
+                    Console.Write("Глубина: ");
+                    if (!double.TryParse(Console.ReadLine(), out double depth)) throw new Exception("Неверный формат глубины.");
+
+                    Console.Write("Вес: ");
+                    if (!double.TryParse(Console.ReadLine(), out double weight)) throw new Exception("Неверный формат веса.");
+
+                    Console.Write("Дата производства (гггг-мм-дд): ");
+                    string dateInput = Console.ReadLine();
+                    if (!DateTime.TryParseExact(dateInput, "yyyy-MM-dd", null, DateTimeStyles.None, out var manufactureDate))
+                        throw new Exception("Неверный формат даты. Используйте формат гггг-мм-дд.");
+
+                    if (width > pallet.Width || depth > pallet.Depth)
+                        throw new Exception("Размеры коробки превышают размеры паллеты по ширине или глубине.");
+
+                    var newBox = new Box
+                    {
+                        Width = width,
+                        Height = height,
+                        Depth = depth,
+                        Weight = weight,
+                        ManufactureDate = manufactureDate
+                    };
+
+                    pallet.Boxes.Add(newBox);
+                    Console.WriteLine("Коробка добавлена.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Ошибка: {ex.Message}");
+                }
+            }
+            else Console.WriteLine("Неверный выбор паллеты.");
+        }
+
+        static void RemoveBoxFromPallet(List<Pallet> pallets)
+        {
+            Console.WriteLine("Выберите паллету:");
+            for (int i = 0; i < pallets.Count; i++)
+                Console.WriteLine($"{i + 1}. ID: {pallets[i].Id}");
+
+            if (int.TryParse(Console.ReadLine(), out int delIndex) && delIndex > 0 && delIndex <= pallets.Count)
+            {
+                var pallet = pallets[delIndex - 1];
+                for (int i = 0; i < pallet.Boxes.Count; i++)
+                    Console.WriteLine($"{i + 1}. Коробка {i + 1}, вес: {pallet.Boxes[i].Weight} кг");
+
+                Console.Write("Введите номер коробки для удаления: ");
+                if (int.TryParse(Console.ReadLine(), out int boxIndex) && boxIndex > 0 && boxIndex <= pallet.Boxes.Count)
+                {
+                    pallet.Boxes.RemoveAt(boxIndex - 1);
+                    Console.WriteLine("Коробка удалена.");
+                }
+                else Console.WriteLine("Неверный номер коробки.");
+            }
+            else Console.WriteLine("Неверный выбор паллеты.");
         }
 
         static void Main()
